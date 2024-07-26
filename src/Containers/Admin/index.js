98451f4c97da20f2650478482219b04332813fd0
pablomartinsti <pablomartinsti@outlook.com>
2024-07-26 11:33:57 -0300

    construindo a table do admin

--- src/Containers/Admin/index.js
+++ src/Containers/Admin/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import { Container } from './style'
+import Orders from './Ordes'
 
 export function Admin() {
   return (
     <Container>
-      <h1>Admin</h1>
+      <Orders />
     </Container>
   )
 }
