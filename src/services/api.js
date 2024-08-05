d5bd10b4cac17625933a1baed50e52c4ee68853e
pablomartinsti <pablomartinsti@outlook.com>
2024-08-05 15:23:47 -0300

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiCodeBurger = axios.create({
-    baseURL: 'https://devburger-api.onrender.com/'
+    baseURL: 'http://localhost:3001'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
